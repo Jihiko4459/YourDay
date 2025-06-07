632e296f12273be3c0a7896d1f2dd5dd16216a1e
Jihiko4459 <anna.demyanenko.4459@mail.ru>
2025-06-07 21:41:11 +0500

    Adding basic settings

--- app/src/main/java/com/example/yourday/MainActivity.kt
+++ app/src/main/java/com/example/yourday/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.yourday
 
+import YourDayTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.yourday.ui.theme.YourDayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/yourday/ui/theme/Color.kt
+++ app/src/main/java/com/example/yourday/ui/theme/Color.kt
@@ -2,10 +2,21 @@ package com.example.yourday.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Red=Color(0xFFE06B53)
+val White=Color(0xFFFFFFFF)
+val Black= Color(0xFF000000)
+val Blue= Color(0xFF53C9E0)
+val Mint=Color(0xFF53E0B1)
+val Gray1= Color(0xFFAEAEAE)
+val Gray2=Color(0xFFF5F5F5)
+val Green= Color(0xFF82E053)
+val Yellow= Color(0xFFE0B153)
+val Primary= Color(0xFFB153E0)
+val Purple1= Color(0xFFF2E2FA)
+val Purple2= Color(0xFFEBD4F7)
+val Purple3= Color(0xFFDDA6FF)
+val Purple4= Color(0xFF731D9E)
+val Purple5= Color(0xff7E1FAD)
+val DarkBlue= Color(0xFF25242E)
+
