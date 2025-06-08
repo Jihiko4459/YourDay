d9a57f9d69bccf3bfbc9b566ad50c74361f63c97
Jihiko4459 <anna.demyanenko.4459@mail.ru>
2025-06-08 19:03:37 +0500

    Create Splash screen

--- app/src/main/java/com/example/yourday/MainActivity.kt
+++ app/src/main/java/com/example/yourday/MainActivity.kt
@@ -1,29 +1,55 @@
 package com.example.yourday
 
 import YourDayTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import android.os.Bundle
+import androidx.compose.runtime.LaunchedEffect
+import kotlinx.coroutines.delay
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.yourday.ui.theme.Primary
+import com.example.yourday.ui.theme.White
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContent {
             YourDayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "splash") {
+                    composable("splash") {
+                        SplashScreen(navController)
+                    }
+                    composable("main") {
+                        // Ваш основной экран
+
+                        MainScreen("Android")
+                    }
                 }
             }
         }
@@ -31,17 +57,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SplashScreen(navController: NavHostController) {
+    Box(
+        modifier = Modifier.fillMaxSize().background(Primary),
+        contentAlignment = Alignment.Center,
+
+    ) {
+        // Вертикальное расположение текста
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "App Logo",
+                modifier = Modifier.size(200.dp), // Размер изображения
+                contentScale = ContentScale.Fit // Масштабирование
+            )
+        }
+    }
+    // Переход через 2 секунды
+    LaunchedEffect(Unit) {
+        delay(2000)
+        navController.navigate("main") {
+            popUpTo(navController.graph.startDestinationId) {
+                inclusive = true
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     YourDayTheme {
-        Greeting("Android")
+    }
+}
+
+@Composable
+fun MainScreen(name: String) {
+    // Управление системными барами
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(
+        color = Color.Transparent,
+        darkIcons = true
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(White)
+            .windowInsetsPadding(WindowInsets.systemBars)
+    ) {
+        // Первый элемент в Column будет размещен вверху
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier
+                .padding(top = 16.dp) // Отступ от верхнего края
+                .padding(horizontal = 16.dp) // Горизонтальные отступы
+        )
     }
 }
